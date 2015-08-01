f2e30db8189021ef798c5007772b63becf54e9b7
Javi Velasco <javier.velasco86@gmail.com>
2015-08-01 19:37:40 +0200

    Reduce gruntfile to svg usage

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,57 +4,6 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    connect: {
-      server: {
-        options: {
-          port: 8080,
-          base: '_site'
-        }
-      }
-    },
-
-    shell: {
-      jekyllBuild: {
-        command: 'jekyll build'
-      }
-    },
-
-    sass: {
-      options: {
-        sourceMap: true
-      },
-      dist: {
-        files: {
-          '_site/assets/css/global-unprefixed.css': 'scss/global.scss'
-        }
-      }
-    },
-
-    autoprefixer: {
-      global: {
-        src: '_site/assets/css/global-unprefixed.css',
-        dest: '_site/assets/css/global.css'
-      }
-    },
-
-    watch: {
-      options: {
-        livereload: true
-      },
-      site: {
-        files: ['index.html', '_layouts/*.html', '_posts/*.md', '_includes/*.html'],
-        tasks: ['shell:jekyllBuild']
-      },
-      css: {
-        files: ['scss/*.scss'],
-        tasks: ['sass', 'autoprefixer']
-      },
-      svgIcons: {
-        files: ['svg/*.svg'],
-        tasks: ['svgstore', 'shell:jekyllBuild']
-      }
-    },
-
     svgstore: {
       options: {
         prefix: 'shape-',
@@ -68,26 +17,9 @@ module.exports = function(grunt) {
           '_includes/svg-defs.svg': ['svg/*.svg']
         }
       }
-    },
-
-    buildcontrol: {
-      options: {
-        dir: '_site',
-        commit: true,
-        push: true,
-        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
-      },
-      pages: {
-        options: {
-          remote: 'git@github.com:javivelasco/javivelasco.github.io.git',
-          branch: 'gh-pages'
-        }
-      }
     }
-
   });
 
   require('load-grunt-tasks')(grunt);
-  grunt.registerTask('deploy', ['buildcontrol:pages']);
-  grunt.registerTask('default', ['shell:jekyllBuild', 'sass', 'autoprefixer', 'svgstore', 'connect', 'watch']);
+  grunt.registerTask('svg', ['svgstore']);
 };
